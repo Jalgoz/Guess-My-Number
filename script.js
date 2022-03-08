53debb644fe2d531ad264c0c398911b5c76fad56
Jalgoz <jalgoz95@gmail.com>
2022-03-07 21:42:03 -0400

    Se termino el juego de Guess My Number

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+let score = 20;
+let highScore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+// document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  switch (score) {
+    case 0:
+      document.querySelector('.message').textContent = '💥 You lost tha game!';
+      document.querySelector('body').style.backgroundColor = 'red';
+      document.querySelector('.number').style.width = '30rem';
+      break;
+    default:
+      if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!';
+      } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+          highScore = score;
+          document.querySelector('.highscore').textContent = highScore;
+        }
+      } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too high!';
+        score--;
+      } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉 Too low!';
+        score--;
+      }
+  }
+
+  document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
